dde7e961826fac59057b13dfa311f5e20c18ebc4
SURAMERICANA\ivanloqu <idlondonoq@sura.com.co>
2019-03-12 14:21:02 -0500

    Adding Monads construction example from scratch.

---
+++ src/test/scala/monads/MyWrapperTest.scala
@@ -0,0 +1,20 @@
+package monads
+
+import functions.Utils
+import org.scalatest.FunSuite
+
+class MyWrapperTest extends FunSuite{
+
+  test("Using MyWrapper monad in a for expression") {
+
+    val result = for {
+      a <- Utils.duplicate(1)
+      b <- Utils.triplicate(a)
+      c <- Utils.quadruple(b)
+    } yield a + b + c
+
+    assert(result.getClass === MyWrapper(32).getClass)
+    assert(result.toString.equals(MyWrapper(32).toString))
+
+  }
+}
